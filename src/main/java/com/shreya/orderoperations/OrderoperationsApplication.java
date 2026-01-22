ac5dfee0bb08f1447ad903d6caff6192275ef6d3
Shreyamehta26 <shreya26.mehta@gmail.com>
2026-01-22 14:51:44 +0530

    Day 1: Springboot setup and first hello endpoint

---
+++ src/main/java/com/shreya/orderoperations/OrderoperationsApplication.java
@@ -0,0 +1,13 @@
+package com.shreya.orderoperations;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class OrderoperationsApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(OrderoperationsApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/shreya/orderoperations/OrderoperationsApplicationTests.java
@@ -0,0 +1,13 @@
+package com.shreya.orderoperations;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class OrderoperationsApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
